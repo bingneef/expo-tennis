b411614329fee0a59ea28ae8abd75f8570434015
Bing Steup <bingsteup@gmail.com>
2017-10-13 16:42:41 +0200

    Update resolvers

--- services/graphql/schema/resolvers/newsItem.js
+++ services/graphql/schema/resolvers/newsItem.js
@@ -3,14 +3,28 @@ import { NewsItem, NewsItemLoader} from'../../../../models/NewsItem'
 export default {
   Query: {
     newsItemById: async (root, { newsItemId }) => await NewsItemLoader.load(newsItemId),
-    newsItems: async (root, { featured, tag, cursor, limit }) => {
+    newsItems: async (root, { featured, tag, notTag, cursor, limit }) => {
       let params = { featured }
       if (tag) {
         params = {
           ...params,
-          tags: tag,
+          tags: {
+            ...params.tags,
+            $eq: tag,
+          },
         }
       }
+
+      if (notTag) {
+        params = {
+          ...params,
+          tags: {
+            ...params.tags,
+            $ne: notTag,
+          },
+        }
+      }
+
       const feed = await NewsItem.find(params).sort({pubDate: -1}).skip(cursor).limit(limit)
       const totalCount = await NewsItem.count(params).where(params)
 
